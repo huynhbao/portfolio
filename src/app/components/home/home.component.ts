c53823d5f3b368cfb2a5be2968322294e80bd821
unknown <huynhbao162000@gmail.com>
2022-11-29 23:20:45 +0700

    .

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+declare const AOS:any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  
+}
